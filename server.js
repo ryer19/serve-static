236c6864d0e1fd18937c3aaf0b1c93519f8e3007
Robert Smiles <robertsmiles1@Madelines-MacBook-Pro-2.local>
2018-08-30 21:27:31 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(3050, () => {
+    console.log('listening on port 3050')
+})
+
